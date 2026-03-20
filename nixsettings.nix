987f06cff908dd1a745cfa3723212da02e3739a5
rysieko <rrx9506@gmail.com>
2026-03-20 18:52:55 +0100

    small refactor that should not break anyrhing but still will

--- nixsettings.nix
+++ nixsettings.nix
@@ -1,9 +1,17 @@
 {config , inputs, ... }:
-
 {
- nix.settings = {
-        substituters = ["https://hyprland.cachix.org" "https://attic.xuyh0120.win/lantian"];
-        trusted-substituters = ["https://hyprland.cachix.org" "https://attic.xuyh0120.win/lantian"];
-        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="  ];
+ nix = { 
+   gc = { 
+       automatic = true;
+       dates = "daily";
+       options = "--delete-older-than 15d";
+   };
+   settings = {
+       experimental-features = [ "nix-command" "flakes" ];
+       auto-optimise-store = true;
+       substituters = ["https://hyprland.cachix.org" "https://attic.xuyh0120.win/lantian"];
+       trusted-substituters = ["https://hyprland.cachix.org" "https://attic.xuyh0120.win/lantian"];
+       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="  ];
+   };
  };
 }
