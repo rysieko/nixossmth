86497cb3bc1222d3b813c9d2ce938df45ed630cf
rysieko <rrx9506@gmail.com>
2026-03-20 09:32:00 +0100

    Add pkgs.nix to imports in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./pkgs.nix
       inputs.home-manager.nixosModules.default
     ];
   services.displayManager.ly.enable = true; # TRUE
