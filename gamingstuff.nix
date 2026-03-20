d3bc4825747b2ddd33bf4dcc34985b9de32b5dfe
rysieko <rrx9506@gmail.com>
2026-03-20 09:20:29 +0100

    Fix overlay definition syntax in gamingstuff.nix

--- gamingstuff.nix
+++ gamingstuff.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs ,... }: 
 { # destructure module args by 'importing' pkgs - only needed when defining a protonPackage
           
-  nixpkgs.overlays = with inputs [
+  nixpkgs.overlays = with inputs; [
     nix-gaming-edge.overlays.default
     nix-gaming-edge.overlays.proton-cachyos
     nix-cachyos-kernel.overlays.default    
