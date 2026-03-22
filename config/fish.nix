f813a9e168c4103cffdca75d161fd0a59f9f054a
rysieko <rrx9506@proton.me>
2026-03-22 19:53:18 +0100

    fish

--- config/fish.nix
+++ config/fish.nix
@@ -5,5 +5,10 @@
     interactiveShellInit = ''
       set fish_greeting ULTRAKILLLLLLLLLLLLLLLLLLL
     '';
+    plugins = [
+        {name = git; src = pkgs.fishPlugins.plugin-git}
+    ]
+                
+    
   };
 }
