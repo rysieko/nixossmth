126506a27ec2fa45edf7c0bd8139d6e4e5370de1
rysieko <rrx9506@proton.me>
2026-04-24 08:40:28 +0200

    vesktop (:

--- hosts/nixsolvesthis/home.nix
+++ hosts/nixsolvesthis/home.nix
@@ -12,7 +12,18 @@
   nixpkgs.config.allowUnfree = true; # Home Manager needs a bit of information about you and the paths it should # manage.
   home.username = "rysieko";
   home.homeDirectory = "/home/rysieko";
-  programs.vesktop.enable = true;
+  programs.vesktop = {
+    enable = true;
+    vencord.settings = {
+      plugins = {
+        MessageLogger = {
+          enabled = true;
+          ignoreSelf = true;
+        };
+        FakeNitro.enabled = true;
+      };
+    };
+  };
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true; # Optional: enable if using X11 apps
@@ -93,27 +104,27 @@
       text = rgb(${color.base05-rgb-r},${color.base05-rgb-g},${color.base05-rgb-b})
     '';
     ".config/fastfetch/config.jsonc".text = ''
-              {
-              "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/master/doc/json_schema.json", 
-              "modules": [
-                "title",
-                "separator",
-                "os",
-                "host",
-                "kernel",
-                "shell",
-                "wm",
-                "terminal",
-      333          "cpu",
-                "gpu",
-                "memory",
-                "localip",
-                ],
-              "logo": {
-              "type": "kitty",        // Logo type: auto, builtin, small, file, etc.
-              "source": "${FETCHLOGO}",      // Built-in logo name or file path
-              }  
-            }'';
+        {
+        "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/master/doc/json_schema.json", 
+        "modules": [
+          "title",
+          "separator",
+          "os",
+          "host",
+          "kernel",
+          "shell",
+          "wm",
+          "terminal",
+          "cpu",
+          "gpu",
+          "memory",
+          "localip",
+          ],
+        "logo": {
+        "type": "kitty",        // Logo type: auto, builtin, small, file, etc.
+        "source": "${FETCHLOGO}",      // Built-in logo name or file path
+        }  
+      }'';
   };
   home.sessionVariables = {
     mainMod = "SUPER";
