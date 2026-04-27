a1e3c7ec3a26a3f1a74ac61e863a3142ef18fa7f
Rysieko <rrx9506@gmail.com>
2026-04-27 13:16:18 +0200

    idk

--- lua/scripts.lua
+++ lua/scripts.lua
@@ -9,3 +9,16 @@ hl.on("layer.closed",function(l)
         hl.exec_cmd("aww-daemon")
     end 
 end)
+hl.on("window.active", function (w)
+    if w.namespace == "gamescope" then
+        hl.exec_cmd("pkill -9 aww-daemon")
+        hl.dsp.toggle_special("game")
+        hl.dsp.move({ 0.0 })
+    end
+end)
+hl.on("window.kill",function (w)
+    if w.namespace == "gamescope" then
+        hl.exec_cmd("aww-daemon")
+        hl.dsp.toggle_special("game")
+    end
+end)    
