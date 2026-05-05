d6a12ecf175d3a29ae698941a13e65dc0c376639
rysieko <rrx9506@proton.me>
2026-05-05 13:46:36 +0200

    The Field of Hopes And Dreams

--- hosts/nixserver/music.nix
+++ hosts/nixserver/music.nix
@@ -3,25 +3,16 @@
   pkgs,
   ...
 }: {
-  services.minecraft-server = {
+  services.navidrome = {
     enable = true;
-    eula = true;
     openFirewall = true;
-    declarative = true;
-    whitelist = {
-      rysieko = "3417b490-fe2c-4f78-98e1-a697a99be16d";
+    settings = { 
+      MusicFolder = "/navidrome/music";
+      PlaylistPath = "/navidrome/playlist";
+      DefaultLanguage = "pl";
+      DefaultTheme = "dark"; 
+      EnableTranscodingCancellation = true;
     };
-    serverProperties = {
-      server-port = 25526;
-      difficulty = 3;
-      gamemode = 0;
-      max-players = 20;
-      force-gamemode = true;
-      level-seed = "123asddq2q231231231adsd";
-      motd = "Im John. John Minecraft";
-      white-list = true;
-      allow-cheats = false;
-    };
-    jvmOpts = "-Xms2048M -Xmx2048M";
+
   };
 }
